b81610bb122c40da46f102c9a1da10167aa42c61
alainkatayi <alainkatayi01@gmail.com>
2025-07-25 09:21:41 +0100

    Ajout du lien pour voir un article en details dans la card article

--- src/app/pages/article-card/article-card.component.ts
+++ src/app/pages/article-card/article-card.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { Article } from '../../core/models/article';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-article-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './article-card.component.html',
   styleUrl: './article-card.component.css'
 })
